e333ffdc97e74dd9c57b6d38858673d9935dae24
Thorose00 <thomassepetys@gmail.com>
2025-05-02 14:31:35 +0200

    fonts and phone

---
+++ Assets/Art/2D/Scripts/PhoneAndTutorialManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using TMPro;
+
+public class PhoneAndTutorialManager : MonoBehaviour
+{
+    public GameObject phoneUI;
+    public TextMeshProUGUI tutorialText;
+    public float tutorialDuration = 15f;
+
+    private float tutorialTimer;
+    private bool tutorialActive = true;
+    private bool phoneIsActive = false;
+
+    void Start()
+    {
+        phoneUI.SetActive(false); // Hide phone on start
+        tutorialText.gameObject.SetActive(true);
+        tutorialTimer = tutorialDuration;
+    }
+
+    void Update()
+    {
+        // Tutorial countdown (only if not hidden by phone)
+        if (tutorialActive && !phoneIsActive)
+        {
+            tutorialTimer -= Time.deltaTime;
+            if (tutorialTimer <= 0f)
+            {
+                tutorialText.gameObject.SetActive(false);
+                tutorialActive = false;
+            }
+        }
+
+        // Toggle phone on P key press
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            phoneIsActive = !phoneIsActive;
+            phoneUI.SetActive(phoneIsActive);
+
+            // Handle tutorial text visibility
+            if (phoneIsActive)
+            {
+                tutorialText.gameObject.SetActive(false);
+            }
+            else if (tutorialActive)
+            {
+                tutorialText.gameObject.SetActive(true);
+            }
+        }
+    }
+}
