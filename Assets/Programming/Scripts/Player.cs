07c5613a36da6cb00d1128131f6dffa124c069d8
Thorose00 <thomassepetys@gmail.com>
2025-05-13 14:40:53 +0200

    zome stuff

--- Assets/Programming/Scripts/Player.cs
+++ Assets/Programming/Scripts/Player.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Cinemachine;
-using UnityEditor.MPE;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
